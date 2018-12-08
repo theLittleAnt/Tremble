e6c5b7619c4032e73cfd5fafb16405c3919d3a21
huangyaqin <ms.y.smileatlife@qq.com>
2018-12-08 15:21:00 +0800

    为了打成jar包做的修改，再运行maven clean 和 install就会打包

--- src/main/java/platform/cars/MainApplication.java
+++ src/main/java/platform/cars/MainApplication.java
@@ -3,14 +3,22 @@ package platform.cars;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @MapperScan("platform.cars.dao")
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
+	}
+
+	@Override//为了打包springboot项目
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
