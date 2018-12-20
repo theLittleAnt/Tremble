7813f3ff89d3b452acf5f9542c9dccfdddd7fe5b
hyq <ms.y.smileatlife@qq.com>
2018-12-20 15:59:54 +0800

    fiddler抓包可以看大https请求中的请求体，所以暂时不考虑使用https

--- src/main/java/platform/cars/configuration/HttpsConfiguration.java
+++ src/main/java/platform/cars/configuration/HttpsConfiguration.java
@@ -1,41 +1,41 @@
 package platform.cars.configuration;
-
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
+//
+//import org.apache.catalina.Context;
+//import org.apache.catalina.connector.Connector;
+//import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+//import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//原本用于对登陆注册请求使用https，但是测试发现https抓包似乎也可以被看到传输字段。暂时不用
+//@Configuration
 public class HttpsConfiguration {
-
-    @Bean
-    public Connector connector(){
-        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8080);//配置http的端口为8080
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
-    }
-
-    @Bean
-    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
-        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint=new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection=new SecurityCollection();
-                collection.addPattern("/user/login");  //登陆和注册请求会重定向到https
-                collection.addPattern("/user/register");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(connector);
-        return tomcat;
-    }
+//
+//    @Bean
+//    public Connector connector(){
+//        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(8080);//配置http的端口为8080
+//        connector.setSecure(false);
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
+//
+//    @Bean
+//    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+//        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint=new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection=new SecurityCollection();
+//                collection.addPattern("/user/login");  //登陆和注册请求会重定向到https
+//                collection.addPattern("/user/register");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(connector);
+//        return tomcat;
+//    }
 }
